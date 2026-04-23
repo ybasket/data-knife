8e2e5f715d0f6ea699bac0fdb2e6f8492cf56e6e
Yannick Heiber <ybasket42@googlemail.com>
2026-04-23 11:46:40 +0200

    Reformat Converter

--- shared/src/main/scala/dataknife/convert/Converter.scala
+++ shared/src/main/scala/dataknife/convert/Converter.scala
@@ -8,14 +8,20 @@ import fs2.Pipe
 trait Converter[In <: Format: ValueOf, Out <: Format: ValueOf, Config] {
   val inputFormat: In = valueOf[In]
   val outputFormat: Out = valueOf[Out]
+
   def commandName: String = s"${inputFormat.name}2${outputFormat.name}"
   def commandHelp: String = s"Convert ${inputFormat.name.toUpperCase} to ${outputFormat.name.toUpperCase}"
+
   def configOpts: Opts[Config]
+
   def convert(config: Config): Pipe[IO, inputFormat.Data, outputFormat.Data]
+
   def convertByteToByte(
       config: Config,
       inputOptions: inputFormat.InputOptions,
       outputOptions: outputFormat.OutputOptions
   ): Pipe[IO, Byte, Byte] =
-    _.through(inputFormat.read(inputOptions)).through(convert(config)).through(outputFormat.write(outputOptions))
+    _.through(inputFormat.read(inputOptions))
+      .through(convert(config))
+      .through(outputFormat.write(outputOptions))
 }
